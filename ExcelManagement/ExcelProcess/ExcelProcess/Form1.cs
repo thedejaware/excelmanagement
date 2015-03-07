ec8e49accceb9077a05e5b57cc52bd533102bbb2
Orkun BALCI <b.orkun@gmail.com>
2015-03-08 01:30:17 +0200

    OnLoad BindValue event added with copy paste

--- ExcelManagement/ExcelProcess/ExcelProcess/Form1.cs
+++ ExcelManagement/ExcelProcess/ExcelProcess/Form1.cs
@@ -85,9 +85,61 @@ namespace ExcelProcess
 
         }
 
+        private void BindExcelValuesWithoutMacro()
+        {
+            try
+            {
+                string executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string xlsmLocation = Path.Combine(executableLocation, "MacroExcel.xlsm");
+
+                //Run Macro
+                object oMissing = System.Reflection.Missing.Value;
+                Excel.ApplicationClass oExcel = new Excel.ApplicationClass();
+                Excel.Workbooks oBooks = oExcel.Workbooks;
+                Excel._Workbook oBook = null;
+                //oExcel.Visible = true;
+                oBook = oBooks.Open(xlsmLocation, oMissing, oMissing,
+                    oMissing, oMissing, oMissing, oMissing, oMissing, oMissing,
+                    oMissing, oMissing, oMissing, oMissing, oMissing, oMissing);
+
+                // Run the macros.
+
+                //RunMacro(oExcel, new Object[] { "MacroExcel.xlsm!CustomExcelMacro" });
+                // Quit Excel and clean up.               
+                string minuteSecond = DateTime.Now.Minute.ToString() + "_" + DateTime.Now.Second.ToString();
+                string newXlsmLocation = Path.Combine(executableLocation, "MacroExcel" + minuteSecond + ".xlsm");
+                oBook.SaveAs(new Object[] { newXlsmLocation });
+                oBook.Close(false, oMissing, oMissing);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oBook);
+                oBook = null;
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oBooks);
+                oBooks = null;
+                oExcel.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(oExcel);
+                oExcel = null;
+
+                string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source='" + newXlsmLocation + "';Extended Properties=Excel 12.0;";
+                OleDbConnection con = new OleDbConnection(connectionString);
+                OleDbCommand com = new OleDbCommand("SELECT * from [Sheet1$]", con);
+                OleDbDataAdapter adp = new OleDbDataAdapter(com);
+                DataTable dt = new DataTable();
+                adp.Fill(dt);
+                grdExcel.AutoGenerateColumns = true;
+                grdExcel.DataSource = dt;
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show(exc.InnerException.Message);
+            }
+
+
+        }
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            BindExcelValues();
+            BindExcelValuesWithoutMacro();
         }
     }
 }
